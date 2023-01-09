4ad02dcab96d4fce02cd2a118ddfcc8aa472dbc2
Noir-Neko <121248936+Noir-Neko@users.noreply.github.com>
2023-01-09 16:22:29 +0200

    patrolling enemy added! :)
    the enemy patrols between two points ,  if player is in the hot zone the enemy will follow it (wont jump) if the player is inside the attack trigger zone enemy will attack

--- Assets/Scripts/PatrollingEnemyBehavier.cs
+++ Assets/Scripts/PatrollingEnemyBehavier.cs
@@ -6,24 +6,22 @@ public class PatrollingEnemyBehavier : MonoBehaviour
 {
 
     #region Public Variables
-    public Transform rayCast;
-    public LayerMask raycastMask;
-    public float rayCastLength;
     public float minAttackDistance; // min attack distance
-    public float speed;
+    public float moveSpeed;
     public float timer; // time between attacks
     public Transform rightLimit;
     public Transform leftLimit;
+    [HideInInspector]public Transform target;
+    [HideInInspector]public bool inRange;
+    public GameObject hotZone;
+    public GameObject triggerArea;
     #endregion
 
     #region Private Variables
-    private RaycastHit2D hit;
-    private Transform target;
     private Animation anim;
     private float Timer;
     private float distance; // stores the distance between the enemy and player
-    private bool attackMode = false; // enemy enters attack mode
-    private bool inRange;//checks if player is in range
+    private bool attackMode; // enemy enters attack mode
     private bool cooling;//checks if enemy is cooling after attack
     #endregion
 
@@ -38,6 +36,7 @@ public class PatrollingEnemyBehavier : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (!attackMode)
         {
             Move();
@@ -45,27 +44,13 @@ public class PatrollingEnemyBehavier : MonoBehaviour
 
         if(!InsideLimits() && !inRange)
         {
-            SelectTarget();
+            SelectTarget();    
         }
+
         if (inRange)
         {
-            hit = Physics2D.Raycast(rayCast.position, transform.right, rayCastLength, raycastMask);
-            RaycastDebugger();
-        }
-
-        //when player is detected
-        if(hit.collider != null)
-        {
+           
             EnemyLogic();
-        }
-        else if (hit.collider == null)
-        {
-            inRange = false;
-        }
-
-        if (!inRange)
-        {
-            stopAttack();
         }
     }
     void EnemyLogic()
@@ -73,34 +58,33 @@ public class PatrollingEnemyBehavier : MonoBehaviour
         distance = Vector2.Distance(transform.position, target.position);
         if(distance > minAttackDistance)
         {
-            Move();
             stopAttack();
         }
-        else if( minAttackDistance >= distance && !cooling)
+        else if( minAttackDistance > distance && !cooling)
         {
             Attack();
         }
 
         if (cooling)
         {
+            cooldown();
             //anim.setBool("Attack", false);
         }
     }
 
     private void Move()
     {
-       // anim.SetBool("canWalk", true);
+        // anim.SetBool("canWalk", true);
         // if(!anim.GetCurrentAnimationStateInfo(0).IsName("name of attack animation"){}
-
+        
         Vector2 targetPosition = new Vector2(target.position.x, transform.position.y);
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
     }
 
     void Attack()
     {
         timer = Timer;//reset timer when player enter attack range
         attackMode = true; // to check if enemy is still attacking or not
-
         //anim.SetBool("canWalk", false);
         //anim.SetBool("Attack", true);
     }
@@ -112,53 +96,42 @@ public class PatrollingEnemyBehavier : MonoBehaviour
        // anim.SetBool("", false);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.gameObject.tag == "Player")
-        {
-            target = collision.transform;
-            inRange = true;
-            Flip();
-        }
-    }
-    void RaycastDebugger()
-    {
-        if(distance > minAttackDistance)
-        {
-            Debug.DrawRay(rayCast.position, transform.right * rayCastLength, Color.red);
-        }
-        else if(minAttackDistance > distance)
-        {
-            Debug.DrawRay(rayCast.position, transform.right * rayCastLength, Color.green);
-        }
-    }
-
     private bool InsideLimits()
     {
         return transform.position.x > leftLimit.position.x && transform.position.x < rightLimit.position.x;
     }
 
-    private void SelectTarget()
+        void cooldown()
+        {
+            timer -= Time.deltaTime;
+        
+            if(timer<=0 && cooling && attackMode)
+            {
+                cooling = false;
+                timer = Timer;
+            }
+        }
+
+    public void SelectTarget()
     {
         float distanceToLeft = Vector2.Distance(transform.position, leftLimit.position);
         float distanceToRight = Vector2.Distance(transform.position, rightLimit.position);
 
-        if(distanceToLeft > distanceToRight)
+        if (distanceToLeft > distanceToRight)
         {
             target = leftLimit;
         }
-        else
+        else if (distanceToLeft < distanceToRight)
         {
             target = rightLimit;
         }
-
         Flip();
     }
 
-    private void Flip()
+    public void Flip()
     {
         Vector3 rotation = transform.eulerAngles;
-        if(transform.position.x > target.position.x)
+        if (transform.position.x > target.position.x)
         {
             rotation.y = 180f;
         }
@@ -167,5 +140,10 @@ public class PatrollingEnemyBehavier : MonoBehaviour
             rotation.y = 0f;
         }
         transform.eulerAngles = rotation;
+    }
+
+    public void TriggerCooling()
+    {
+        cooling = true;
     }
 }
