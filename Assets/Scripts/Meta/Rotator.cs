16d17744389677370e0a0ef81ea5a5ecd836818f
Shaked <shakedrobinzon@gmail.com>
2023-01-14 15:10:34 +0200

    fixed hud

--- Assets/Scripts/Meta/Rotator.cs
+++ Assets/Scripts/Meta/Rotator.cs
@@ -8,6 +8,6 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime);
     }
 }
